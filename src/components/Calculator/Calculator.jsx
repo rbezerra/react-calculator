0334bc36967b488cb7e7ff29b9bf9f59f5caae1f
Ramon Bezerra <ramonbezerra90@gmail.com>
2019-04-25 23:42:49 -0300

    including Display

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,31 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import Display from "../Display/Display";
+import "./Calculator.css";
 
-class Calculator extends Component{
-    state = {
-        displayValue: 0,
-        numbers : [],
-        operators: [],
-        selectedOperator: '',
-        storedValue: '',
-    };
+class Calculator extends Component {
+  state = {
+    displayValue: 0,
+    numbers: [],
+    operators: [],
+    selectedOperator: "",
+    storedValue: ""
+  };
 
-    callOperator = () => {
-        console.log('call operation');
-    }
+  callOperator = () => {
+    console.log("call operation");
+  };
 
-    setOperator = () => {
-        console.log('set operation');
-    }
+  setOperator = () => {
+    console.log("set operation");
+  };
 
-    updateDisplay = () => {
-        console.log('update display');
-    }
+  updateDisplay = () => {
+    console.log("update display");
+  };
 
-    render(){
-        return (
-            <div className="calculator-container"/>
-        )
-    }
+  render() {
+    const { displayValue } = this.state;
+
+    return (
+      <div className="calculator-container">
+        <Display displayValue={displayValue} />
+      </div>
+    );
+  }
 }
 
 export default Calculator;
--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import Calculator from './Calculator';
+import React from "react";
+import { shallow } from "enzyme";
+import Calculator from "./Calculator";
+import Display from "../Display/Display";
 
-describe('Calculator', () => {
-    let wrapper;
-    beforeEach(() => wrapper = shallow(<Calculator />));
+describe("Calculator", () => {
+  let wrapper;
+  beforeEach(() => (wrapper = shallow(<Calculator />)));
 
-    it('should render a <div/>', () => {
-        expect(wrapper.find('div').length).toEqual(1);
-    });
-})
+  it("should render a <div/>", () => {
+    expect(wrapper.find("div").length).toEqual(1);
+  });
+
+  it("should render the Display component", () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <Display displayValue={wrapper.instance().state.displayValue} />
+      )
+    ).toEqual(true);
+  });
+});
