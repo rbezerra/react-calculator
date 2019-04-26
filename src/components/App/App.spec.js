7e3b70321b393cc9f1144e4b1b25c6a4391e0519
Ramon Bezerra <ramonbezerra90@gmail.com>
2019-04-25 23:21:39 -0300

    include snapshot test fro App.js

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -7,6 +7,10 @@ describe("App", () => {
   let wrapper;
   beforeEach(() => (wrapper = shallow(<App />)));
 
+  it('should render correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+  });
+
   it("should render a <div/>", () => {
     expect(wrapper.find("div").length).toEqual(1);
   });
